45b3067e0afb352c8d2cff0b951df82afa56fe54
ebilgin <enes1988@gmail.com>
2017-01-24 07:53:54 -0600

    Update on L3

---
+++ lectures/lecture-03/houseSample.R
@@ -0,0 +1,23 @@
+sample_house_prices <- c(327000,
+                         276000,
+                         513000, 
+                         216000, 
+                         127000,
+                         413000)
+sample_mean <- mean(sample_house_prices)
+sample_variance <- var(sample_house_prices)
+sample_standard_deviation <- sd(sample_house_prices)
+sample_mean_5 <- mean(sample_house_prices[0:5])
+cat("Sample Mean", sample_mean)
+cat("Sample Variance", sample_variance)
+cat("Sample Standard Deviation", sample_standard_deviation)
+cat("Sample Mean of first 5",sample_mean_5)
+
+house_price_ci_95 <- t.test(sample_house_prices)
+house_price_ci_99 <- t.test(sample_house_prices, conf.level = 0.99)
+
+cat("With 95%, the average price is between:", house_price_ci_95$conf.int)
+cat("With 99%, the average price is between:", house_price_ci_99$conf.int)
+
+
+
